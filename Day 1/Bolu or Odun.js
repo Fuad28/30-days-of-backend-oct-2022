2990cc73b989259091a8f1fee79b602cd5caaa6f
Fuad <fuadadio75@gmail.com>
2022-10-05 21:17:33 +0100

    javascript version

---
+++ Day 1/Bolu or Odun.js
@@ -0,0 +1,19 @@
+function BoO(word){
+    //Checks if a word contains special characters or a space.
+
+    let special_chars= /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+    if ((typeof(word) !== 'string') || (special_chars.test(word)))
+        console.log('Please remove all spaces or special characters');
+
+    else if (['bolu', 'odun'].includes(word.toLowerCase()))
+        console.log(`Welcome back, ${word}`);
+
+    else {console.log(`It is nice to meet you, ${word}`)}
+        
+};
+
+//Tests
+BoO('Micheal Jackson');
+BoO('Micheal');
+BoO('Bolu');
